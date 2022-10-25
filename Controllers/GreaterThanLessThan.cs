95b2629da0b9fd3fb6b5bada311ba27f23db2ee8
Amardeep Mann <amar2slick@gmail.com>
2022-10-25 15:57:49 -0700

    Peer reviewed

--- Controllers/GreaterThanLessThan.cs
+++ Controllers/GreaterThanLessThan.cs
@@ -2,7 +2,7 @@
 // 10-25-22
 // GreaterThanLessThan Endpoint
 // Added the ability to take in an input via web URL for our GreaterThanLessThan function
-// Peer reviewed by
+// Peer reviewed by Carlos Felipe: This code works great. The TryParse method is something new that I hadn't seen from others. The code writes out a correct response and the code looks to be formatted correctly.
 
 using Microsoft.AspNetCore.Mvc;
 
